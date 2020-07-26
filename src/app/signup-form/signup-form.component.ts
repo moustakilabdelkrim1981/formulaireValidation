7401686f92525f2b45df1b073c8565859b291ccc
bouachibi <vincentmoustakil@gmail.com>
2020-07-26 14:39:52 +0200

    formulaire signup

---
+++ src/app/signup-form/signup-form.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormControl,Validators} from '@angular/forms';
+
+@Component({
+  selector: 'signup-form',
+  templateUrl: './signup-form.component.html',
+  styleUrls: ['./signup-form.component.css']
+})
+export class SignupFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  form=new FormGroup({
+   username:new FormControl('',[
+     Validators.required,
+     Validators.minLength(3)
+    ]),
+   password:new FormControl('',[
+    Validators.required,
+    Validators.minLength(6)
+   ])
+ })
+ get username(){
+  return this.form.get('username');
+ }
+ get password(){
+  return this.form.get('password');
+ }
+}
